fcfa194154b9f9065e1a9f23b2fd75c6f6b4a97e
gabrielms-castro <gabrielms.castro@gmail.com>
2025-10-01 17:20:22 -0300

    feat: add readiness endpoint for checking app healthness

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,12 @@
 import express from "express";
 const app = express();
 const PORT = 8080;
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
+app.get("/healthz", handlerReadiness);
+async function handlerReadiness(req, res) {
+    res.set("Content-Type", "text/plain; charset=utf-8");
+    res.status(200).send("OK");
+}
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
