ce573566985ba44d2239a92757e48655ebd865f9
gabrielms-castro <gabrielms.castro@gmail.com>
2025-10-05 11:19:20 -0300

    feat: add remove profane words with removeProfane; add configs.js

---
+++ src/api/configs.ts
@@ -0,0 +1,6 @@
+export const maxChirpsLenght = 140;
+export const profaneWords: Array<string> = [
+    "kerfuffle", 
+    "sharbert", 
+    "fornax"
+]
--- src/api/handlerValidateChirp.ts
+++ src/api/handlerValidateChirp.ts
@@ -1,14 +1,35 @@
 import type { Request, Response } from "express";
+import { profaneWords } from "./configs.js";
 
 export async function handlerValidateChirp(req: Request, res: Response) {
     type parameters = {
         body: string;
     }
     const params: parameters = req.body;
-    const maxChirpsLength = 140;
-    if (params.body.length > maxChirpsLength) {
+    let text = params.body;
+
+    if (!isMaxChirpsLengthValid(text)) {
         res.status(400).send(JSON.stringify({ "error": "Chirp is too long" }))
         return;
     }
-    res.status(200).send(JSON.stringify({ "valid": true }))
+    res.status(200).send(JSON.stringify({ "cleanedBody": removeProfame(text) }))
+}
+
+function isMaxChirpsLengthValid(text: string, maxChirpLength: number = 140): boolean {
+    if (!text) throw new Error(`Invalid text: ${text}`);
+    if (text.length > maxChirpLength) return false;
+    return true;
+}
+
+function removeProfame(text: string): string {
+    const textArray = text.split(" ");
+
+    for (let i=0; i < textArray.length; i++) {
+        let word = textArray[i].toLowerCase()
+        if (profaneWords.includes(word)) {
+            textArray[i] = "****";
+        }
+    }
+    const cleanedText = textArray.join(" ")
+    return cleanedText
 }
