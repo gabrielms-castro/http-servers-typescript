b2e83892453a990762696f3e10f65cbeee4c5b5c
gabrielms-castro <gabrielms.castro@gmail.com>
2025-10-12 08:05:36 -0300

    fix: fixed error handler to throw correct status and message. it was hardcoded

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,16 +1,35 @@
 import { Request, Response, NextFunction } from "express"
-import { BadRequestError } from "../utils/errors.js";
+import { 
+    BadRequestError, 
+    ForbiddenError, 
+    NotFoundError, 
+    UnauthorizedError,
+
+} from "../utils/errors.js";
 
 export async function middlewareErrorHandler(err: Error, _: Request, res: Response, __: NextFunction) {
     console.log(err)
 
+    let statusCode = 500;
+    let message =  "Something went wrong on our end";
+
     if (err instanceof BadRequestError) {
-        return res.status(400).send(JSON.stringify({
-            "error": "Chirp is too long. Max length is 140"
-        }));
-    } else {
-        return res.status(500).send(JSON.stringify({
-            "error": "Something went wrong on our end"
-        }));
+        statusCode = 400;
+        message = err.message
+    } else if ( err instanceof UnauthorizedError) {
+        statusCode = 401;
+        message = err.message;
+    } else if ( err instanceof ForbiddenError) {
+        statusCode = 403;
+        message = err.message;
+    } else if ( err instanceof NotFoundError) {
+        statusCode = 404;
+        message = err.message;
     }
+
+    res.status (400).send(JSON.stringify(
+        {
+            "error": message
+        }
+    ));
 }
